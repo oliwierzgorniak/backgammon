e034f4004362497a666fa9e7b2f5998cc9a2b450
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2022-07-27 15:18:26 +0200

    graphql server set up

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,29 @@
+import "reflect-metadata";
 import express from "express";
 import "dotenv/config";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
-const app = express();
+const main = async () => {
+  const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
 
-app.listen(process.env.PORT, () => {
-  console.log("server is listening on port", process.env.PORT);
-});
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.get("/", (_, res) => {
+    res.send("hello");
+  });
+
+  app.listen(process.env.PORT, () => {
+    console.log("server is listening on port", process.env.PORT);
+  });
+};
+main();
