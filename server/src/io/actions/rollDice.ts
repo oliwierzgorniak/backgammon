d26aff2ca7cf45637ecf27149c05539858b15c2f
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2023-02-01 17:20:19 +0100

    rolling dice rewritten to websockets

---
+++ server/src/io/actions/rollDice.ts
@@ -0,0 +1,28 @@
+import { Socket } from "socket.io";
+import { io } from "../..";
+import redis from "../../clients/redis";
+import getDiceNumber from "./rollDice/getDiceNumber";
+
+export default async function rollDices(socket: Socket) {
+  const username = socket.handshake.auth.username as string | undefined;
+
+  if (!username) {
+    console.error("!username is true");
+    return;
+  }
+
+  const isRollAvailable = await redis.get(username + "-is-roll-available");
+  if (!isRollAvailable || JSON.parse(isRollAvailable)) {
+    console.error("isRollAvailable is null or isRollAvailble is falsy");
+    return;
+  }
+
+  const dice = [getDiceNumber(), getDiceNumber()];
+
+  const userSocketId = await redis.get(username + "-socket-id");
+  if (!userSocketId) {
+    console.error("userSocketId is null");
+    return;
+  }
+  io.to(userSocketId).emit("dice-rolled", dice);
+}
--- server/src/routes/game/game.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import dices from "./handlers/dices";
-
-const router = express.Router();
-
-router.post("/dices", dices);
-
-export default router;
